6a5737686f9b6edacbd0ae91df915c448efff861
Yauheni Kuntsevich <cmaczok@gmail.com>
2026-02-05 00:53:17 +0300

    feat: add int id pk mixin

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 
 from core.config import settings
 
@@ -10,5 +10,3 @@ class Base(DeclarativeBase):
     metadata = MetaData(
         naming_convention=settings.db.naming_convention,
     )
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models.base import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
 
 
-class User(Base):
+class User(IntIdPkMixin, Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(unique=True)
